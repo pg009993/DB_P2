390e58da08fd48ebbc9c0bf78eb56972a1366644
Phillip <pwg1994@gmail.com>
2017-02-07 09:55:54 -0500

    updated homepage

--- homepage.php
+++ homepage.php
@@ -1,23 +1,43 @@
 <!DOCTYPE html>
-<html lang="">
+<html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <link rel="stylesheet" href="project2.css">
+    <title>Database Project 2</title>
 </head>
 
 <body>
-    <form action="dept_w_min_num_employees.php" method="get">
-        <input type="submit" value="List department(s) with minimum number of employees" /> </form>
-    <form action="female_vs_male_salaries.php" method="get">
-        <input type="submit" value="List department(s) with maximum ratio of average female salaries to average men salaries" /> </form>
-    <form action="longest_serving_managers.php" method="get">
-        <input type="submit" value="List manager(s) who holds office for the longest duration" /> </form>
-    <form action="decade_n_salaries.php" method="get">
-        <input type="submit" value="For each department, list number of employees born in each decade and their average salaries" /> </form>
-    <form action="female_pre1990_80k_manager.php" method="get">
-        <input type="submit" value="List employees, who are female, born before Jan 1, 1990, makes more than 80K annually and hold a manager position" /> </form>
+    <br>
+    <div class="buttons">
+        <form action="dept_w_min_num_employees.php" method="get">
+            <input type="submit" value="List department(s) with minimum number of employees" /> 
+        </form>
+    </div>
+    <br>
+    <div class="buttons">
+        <form action="female_vs_male_salaries.php" method="get">
+            <input type="submit" value="List department(s) with maximum ratio of average female salaries to average men salaries" /> 
+        </form>
+    </div>
+    <br>
+    <div class="buttons">
+        <form action="longest_serving_managers.php" method="get">
+            <input type="submit" value="List manager(s) who holds office for the longest duration" /> </form>
+    </div>
+    <br>
+    <div class="buttons">
+        <form action="decade_n_salaries.php" method="get">
+            <input type="submit" value="For each department, list number of employees born in each decade and their average salaries" /> 
+        </form>
+    </div>
+    <br>
+    <div class="buttons">
+        <form action="female_pre1990_80k_manager.php" method="get">
+            <input type="submit" value="List employees, who are female, born before Jan 1, 1990, makes more than 80K annually and hold a manager position" />
+        </form>
+    </div>
 </body>
 
 </html>
