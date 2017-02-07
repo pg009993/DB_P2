f38bb0b062e8053adb5547d86d62ce4266d08532
Phillip <pwg1994@gmail.com>
2017-02-06 21:54:20 -0500

    Buttons added to homepage

--- homepage.php
+++ homepage.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,16 @@
 </head>
 
 <body>
-    
-<p>Hello World!</p>    
-    
-    
-    
+    <form action="dept_w_min_num_employees.php" method="get">
+        <input type="submit" value="List department(s) with minimum number of employees" /> </form>
+    <form action="female_vs_male_salaries.php" method="get">
+        <input type="submit" value="List department(s) with maximum ratio of average female salaries to average men salaries" /> </form>
+    <form action="longest_serving_managers.php" method="get">
+        <input type="submit" value="List manager(s) who holds office for the longest duration" /> </form>
+    <form action="decade_n_salaries.php" method="get">
+        <input type="submit" value="For each department, list number of employees born in each decade and their average salaries" /> </form>
+    <form action="female_pre1990_80k_manager.php" method="get">
+        <input type="submit" value="List employees, who are female, born before Jan 1, 1990, makes more than 80K annually and hold a manager position" /> </form>
 </body>
+
 </html>
