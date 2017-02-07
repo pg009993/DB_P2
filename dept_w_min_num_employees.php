07a6b02beb0c214ec8a5af1f20220d6e2c9d5abe
Jonathan Waring <jwaring8@gmail.com>
2017-02-07 17:42:05 -0500

    updated dept_w_min_num_employees

--- dept_w_min_num_employees.php
+++ dept_w_min_num_employees.php
@@ -1,6 +1,35 @@
 <?php
+//Code for connector pulled from http://www.w3schools.com/php/php_mysql_select.asp
 
+$servername = "localhost";
+$username = "username";
+$password = "password";
 
-echo '•	List department(s) with minimum number of employees'
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT d1.dept_no, d1.dept_name
+       	FROM departments d1
+	WHERE num_emps = MIN(num_emps) IN (
+	      SELECT COUNT (DISTINCT e.emp_no) AS num_emps, d.dept_no, d.dept_name
+       	      FROM departments d2, dept_emp e
+	      GROUP BY d.dept_no);"
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Dept no: " . $row["dept_no"]. " - Name: " . $row["dept_name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 
 >
