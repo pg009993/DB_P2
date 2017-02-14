eab48ebd579dfda60c1997e44a4e1ea50185d70a
Jonathan Waring <jwaring8@gmail.com>
2017-02-14 16:20:50 -0500

    updated query to only show one longest duration manager

--- longest_serving_managers.php
+++ longest_serving_managers.php
@@ -13,9 +13,18 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT e.emp_no, e.first_name, e.last_name, d.from_date, d.to_date
-    	FROM dept_manager AS d JOIN employees AS e ON d.emp_no=e.emp_no
-     	ORDER BY DATEDIFF(d.from_date, d.to_date);"
+$sql = "CREATE TABLE temp_table
+        SELECT e.emp_no, e.first_name, e.last_name, d.from_date, d.to_date
+    	FROM dept_manager AS d JOIN employees AS e ON d.emp_no=e.emp_no;
+     	
+        UPDATE temp_table
+        SET to_date = CURDATE()
+        WHERE to_date = '9999-01-01';
+        
+        SELECT * FROM temp_table
+        ORDER BY DATEDIFF(d.from_date, d.to_date) LIMIT 1;
+        
+        DROP TABLE temp_table;"
 
 $result = $conn->query($sql);
 
