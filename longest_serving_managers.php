21be0a31280175155e30680fd4606210ebec2061
Phillip <pwg1994@gmail.com>
2017-02-16 14:42:00 -0500

    need to test more, to_date may not be working properly

--- longest_serving_managers.php
+++ longest_serving_managers.php
@@ -6,14 +6,8 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         
      //query being passed to the database 
-$sql = "SET SQL_SAFE_UPDATES = 0;
-    
-        UPDATE dept_manager
-        SET to_date = CURDATE()
-        WHERE to_date = '9999-01-01';
-    
-        SELECT e.emp_no, e.first_name, e.last_name, d.from_date, d.to_date
-	FROM dept_manager AS d JOIN employees AS e ON d.emp_no=e.emp_no
+$sql = "SELECT e.emp_no, e.first_name, e.last_name, d.from_date, d.to_date
+	   FROM dept_manager AS d JOIN employees AS e ON d.emp_no=e.emp_no
         ORDER BY DATEDIFF(d.from_date, d.to_date) LIMIT 1;";
         
      //prepping the query and passing it to the databas 
@@ -22,16 +16,15 @@ $sql = "SET SQL_SAFE_UPDATES = 0;
         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $numRows = $stmt->rowCount();
 
-        //echo $numRows;
-            //printing the results to the datbase 
-        if($numRows > 0){
+     
+      if($numRows > 0){
             // output data of each row  
         while($row = $stmt->fetch()) {
-                echo "Manager Number: " . $row["emp_no"] . "First Name: " . $row["first_name"]. "Last Name: " . $row["last_name"]. "Start Date: " . $row["from_date"] . "End Date: ". $row["to_date"]. "<br>";
-                }
+            echo "Manager Number: " . $row["emp_no"] . " First Name: " . $row["first_name"]. " Last Name: " . $row["last_name"]. " Start Date: " . $row["from_date"] . " End Date: ". $row["to_date"]. "<br>";
+            
+        }
             echo '</table>';
-            }
-    else{
+            }else{
             echo 'No results';
             }
         } 
@@ -43,3 +36,6 @@ $sql = "SET SQL_SAFE_UPDATES = 0;
 
 $conn = null;
 ?>
+
+
+
