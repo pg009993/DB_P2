078bfc67ed448861b518fbf4cca2e36f8fef135e
Dizdarevic <dizdarevic1994@gmail.com>
2017-02-17 18:59:24 -0500

    fixins pt2

--- longest_serving_managers.php
+++ longest_serving_managers.php
@@ -20,13 +20,13 @@ $sql = "SELECT e.emp_no, e.first_name, e.last_name, d.from_date, d.to_date
       if($numRows > 0){
             // output data of each row  
         while($row = $stmt->fetch()) {
-            echo "Manager Number: " . $row["emp_no"] . " First Name: " . $row["first_name"]. " Last Name: " . $row["last_name"]. " Start Date: " . $row["from_date"];
+            $allDates = "Manager Number: " . $row["emp_no"] . " First Name: " . $row["first_name"]. " Last Name: " . $row["last_name"]. " Start Date: " . $row["from_date"];
                 
-                if($row[to_date] == '9999-01-01'){
-              "End Date: " . date("Y-m-d");
+            if($row[to_date] == '9999-01-01'){
+                echo $allDates . "End Date: " . date("Y-m-d") . "<br>";
                 }
             else{ 
-                "End Date: ". $row["to_date"]. "<br>";
+                echo $allDates . "End Date: ". $row["to_date"]. "<br>";
             }
             
         }
