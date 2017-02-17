42b5308737954e91b12e87a2cc3556b9bc8d5e72
Phillip <pwg1994@gmail.com>
2017-02-17 18:15:03 -0500

    jay query

--- decade_n_salaries.php
+++ decade_n_salaries.php
@@ -7,10 +7,21 @@ try {$conn = new PDO("mysql:host=" . $servername . ";dbname=" . $dbname, $userna
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         //query being passed to the database
-$sql = "CREATE TABLE temp_table
-        SELECT emp_no, LEFT(CAST(YEAR(birth_date) 
-        AS CHAR(4)), 3) AS birth_decade, first_name, last_name)
-        FROM employees;"; 
+$sql = "CREATE TABLE sal_sum
+SELECT emp_no, SUM(salary) AS salary
+FROM salaries
+GROUP BY emp_no;
+
+SELECT * FROM (
+SELECT d.dept_no, CONCAT(SUBSTR(e.birth_date,1,3),'0') AS decade,
+COUNT(e.birth_date) AS num_emps,
+TRUNCATE((SUM(s.salary)/COUNT(e.birth_date)),2) AS avg_sal
+FROM dept_emp d, employees e, sal_sum s
+WHERE d.emp_no=e.emp_no AND d.emp_no=s.emp_no
+GROUP BY dept_no, CONCAT(SUBSTR(birth_date,1,3),'0')) AS sub_query
+ORDER BY dept_no LIMIT 1;
+
+DROP TABLE sal_sum;"; 
         
      //prepping results
         $stmt = $conn->prepare($sql);
