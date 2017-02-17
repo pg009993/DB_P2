8db25a5df1a755f80e2ceda44082c81e1d6c4aab
Dizdarevic <dizdarevic1994@gmail.com>
2017-02-17 18:43:20 -0500

    fixins

--- dept_w_min_num_employees.php
+++ dept_w_min_num_employees.php
@@ -8,7 +8,7 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "SELECT d.dept_name, d.dept_no, COUNT(e.emp_no) AS num_emps
     	FROM departments AS d JOIN dept_emp AS e ON d.dept_no=e.dept_no
         GROUP BY e.dept_no
-    	ORDER BY num_emps LIMIT 1;";
+    	ORDER BY num_emps;";
         
      //prepping the results of the query 
         $stmt = $conn->prepare($sql);
@@ -16,14 +16,13 @@ $sql = "SELECT d.dept_name, d.dept_no, COUNT(e.emp_no) AS num_emps
         $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
         $numRows = $stmt->rowCount();
 
-
      //printing the results
      if($numRows > 0){
             // output data of each row  
-        while($row = $stmt->fetchAll()) {
+        
+        while($row = $stmt->fetch()) {
         echo "Dept no: " . $row["dept_no"]. "Dept Name: " . $row["dept_name"]. "Number of Employees: " . $row["num_emps"] . "<br>";
                 }
-            echo '</table>';
             }else{
             echo 'No results';
             }
