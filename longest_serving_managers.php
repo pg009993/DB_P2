75cdc75ee171c3f9d5e613379b498ef287c31389
Phillip <pwg1994@gmail.com>
2017-02-17 18:51:39 -0500

    current date

--- longest_serving_managers.php
+++ longest_serving_managers.php
@@ -8,7 +8,7 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      //query being passed to the database 
 $sql = "SELECT e.emp_no, e.first_name, e.last_name, d.from_date, d.to_date
 	   FROM dept_manager AS d JOIN employees AS e ON d.emp_no=e.emp_no
-        ORDER BY DATEDIFF(d.from_date, d.to_date) LIMIT 1;";
+        ORDER BY DATEDIFF(d.from_date, d.to_date);";
         
      //prepping the query and passing it to the databas 
         $stmt = $conn->prepare($sql);
@@ -20,7 +20,14 @@ $sql = "SELECT e.emp_no, e.first_name, e.last_name, d.from_date, d.to_date
       if($numRows > 0){
             // output data of each row  
         while($row = $stmt->fetch()) {
-            echo "Manager Number: " . $row["emp_no"] . " First Name: " . $row["first_name"]. " Last Name: " . $row["last_name"]. " Start Date: " . $row["from_date"] . " End Date: ". $row["to_date"]. "<br>";
+            echo "Manager Number: " . $row["emp_no"] . " First Name: " . $row["first_name"]. " Last Name: " . $row["last_name"]. " Start Date: " . $row["from_date"];
+                
+                if($row[to_date] == '9999-01-01'){
+              "End Date: " . date("Y-m-d");
+                }
+            else{ 
+                "End Date: ". $row["to_date"]. "<br>";
+            }
             
         }
             echo '</table>';
