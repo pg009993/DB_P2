f5bf554fa1ba65a897e1e1b5f02d35b136362705
ashetonh <ashetoncharrell@gmail.com>
2017-02-18 14:38:30 -0500

    Comments added

--- customQuery.php
+++ customQuery.php
@@ -11,44 +11,43 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
      //query being passed to the database 
      $sql = "SELECT * FROM ".$table1 . " NATURAL JOIN ".$table2." LIMIT 100;";
      
-     //$sql2 = "DESCRIBE " .$table1 . " NATURAL JOIN ". $table2 .";";
     
-                //prepping the query and passing it to the database 
+    //prepping the query and passing it to the database 
      $stmt = $conn->prepare($sql);
      $stmt->execute();
      $result = $stmt->setFetchMode(PDO::FETCH_NUM);
      $numRows = $stmt->rowCount();
-     $numCols = $stmt->columnCount();
+     $numCols = $stmt->columnCount(); //get number of columns
 
-     $fields = array_keys($stmt->fetch(PDO::FETCH_ASSOC));
+     $fields = array_keys($stmt->fetch(PDO::FETCH_ASSOC)); // fill array with column names
 
 
 
             //printing the results to the datbase 
-        if($numRows > 0){
-            echo '<table>';
-            for($z=0;$z<sizeof($fields);$z++){
-                echo '<th>' . $fields[$z] . '</th>';
-            }
-            while($row = $stmt->fetch()){
-                echo '<tr>';
-            // output data of each row
-                for($i=0;$i<$numCols;$i++){
-                    echo '<td>' . $row[$i] . '</td>';
-                } // for
-                echo '</tr>';
-            } // while
-            
-            
-        echo '</table>';
-            }
-    else{
-            echo 'No results';
-            }
-        } 
+     if($numRows > 0){ // run if there are rows to be fetched. Otherwise, show no results
+        echo '<table>';
+        for($z=0;$z<sizeof($fields);$z++){ // add headers for table
+            echo '<th>' . $fields[$z] . '</th>';
+        }
+        while($row = $stmt->fetch()){ // iterate through and put tuples into table
+            echo '<tr>'; // beginning of row.
+        // output data of each row
+            for($i=0;$i<$numCols;$i++){ // putting table data in each row.
+                echo '<td>' . $row[$i] . '</td>';
+            } // for
+            echo '</tr>'; // end of row
+        } // while
 
-//error handling 
-        catch (PDOException $e) {
+
+            echo '</table>'; //
+        }
+        else{
+            echo 'No results';
+        }
+    } 
+
+    // error handling 
+    catch (PDOException $e) {
             die('Database connection failed: ' . $e->getMessage());
         }
 
